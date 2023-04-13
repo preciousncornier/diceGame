55b1ca25e415909b0582cbb358a39e3075e412f0
Nina <preciouscornier13@gmail.com>
2023-04-13 18:01:47 -0400

    update

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-var randomNumber1 = Math.floor(Math.random() * 6);
-if (randomNumber1 === 1) {
-    if (randomNumber1 === 2) {
-        if (randomNumber1 === 3) {
-            if (randomNumber1 === 4) {
-                if (randomNumber1 === 5) {
-                    if (randomNumber1 === 6) {
-                        document.querySelector(".dice .img1").src="/dice6.png";
-                    }
-                    document.querySelector(".dice .img1").src="/dice5.png";
-                }
-                document.querySelector(".dice .img1").src="/dice4.png";
-            }
-            document.querySelector(".dice .img1").src="/dice3.png";
-        }
-        document.querySelector(".dice .img1").src="/dice2.png";
-    }
-    document.querySelector(".dice .img1").src="/dice1.png";
-}
+var randomNumber1 = Math.floor(1 + Math.random() * 6);
+document.querySelector(".img1").setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(1 + Math.random() * 6);
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
